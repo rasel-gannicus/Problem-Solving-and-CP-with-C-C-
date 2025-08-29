67cb1692d2f833a89ebd6d132fa6b823bf69892f
shafik720 <shafikrasel5@gmail.com>
2025-08-29 21:59:15 +0600

    solving problem

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    int main(){
+        char a,b,c;
+        scanf("%c%c%c",&a,&b,&c);
+        int abc = (a - '0') * 100 + (b - '0') * 10 + (c - '0') ;
+        int bca = (b - '0') * 100 + (c - '0') * 10 + (a - '0') ;
+        int cab = (c - '0') * 100 + (a - '0') * 10 + (b - '0') ;
+        printf("%d\n", abc + bca + cab);
+        return 0 ;
+    }
---
+++ test.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+    int main(){
+        char a = '5' ;
+        int b = a - '0' ; 
+        return 0;
+    }
